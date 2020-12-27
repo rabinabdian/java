309d57d921ef95f75b8c05df988976321a07ad6f
rabinabdian <rabin.abdian@gmail.com>
2020-12-27 13:37:16 +0200

    compute value of x raised to power of y

---
+++ ex1/src/powerEx/calculatePower.java
@@ -0,0 +1,36 @@
+package powerEx;
+
+import java.util.Scanner;
+
+public class calculatePower {
+
+	public static void main(String[] args) {
+		// compute value of x raised to power of y 
+		
+	    System.out.println("compute value of x raised to power of y");
+	    System.out.println ("Enter Number X : ");
+	    Scanner sc = new Scanner(System.in);
+	    int num = sc.nextInt();
+		
+	    System.out.println ("Enter Power Y : ");
+	    int pow = sc.nextInt();
+
+
+	    calculatePow(num,pow);
+	    
+	    sc.close();
+	    
+	}
+
+	private static void calculatePow(int num, int pow) {
+		// TODO Auto-generated method stub
+		int temp = num;
+		for (int i = 1; i < pow; i++) {
+			temp *=num;
+		}
+		System.out.println(" value of "+num+ "raised to power of "+pow+" is " + temp);
+	}
+	
+
+
+}
