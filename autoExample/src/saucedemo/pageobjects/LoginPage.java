d12e0ad88e89f125e8f10bec93aa51c86d0af919
rabinabdian <rabin.abdian@gmail.com>
2020-12-29 15:26:06 +0200

    update working with page factory

--- autoExample/src/saucedemo/pageobjects/LoginPage.java
+++ autoExample/src/saucedemo/pageobjects/LoginPage.java
@@ -3,19 +3,23 @@ package saucedemo.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
 
+	@FindBy(css = "#user-name")
 	WebElement userName;
+	@FindBy(css = "#password")
 	WebElement password;
+	@FindBy(css = "#login-button")
 	WebElement loginBtn;
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
-
-		userName = driver.findElement(By.cssSelector("#user-name"));
-		password = driver.findElement(By.cssSelector("#password"));
-		loginBtn = driver.findElement(By.cssSelector("#login-button"));
+//
+//		userName = driver.findElement(By.cssSelector("#user-name"));
+//		password = driver.findElement(By.cssSelector("#password"));
+//		loginBtn = driver.findElement(By.cssSelector("#login-button"));
 	}
 	
 	// actions 
